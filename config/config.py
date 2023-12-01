f3b4394ba559ff67e2618381949707a313307a55
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2023-12-01 20:29:02 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -47,7 +47,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Pranav-Saraswat/Testing-Alpha",
+    "https://github.com/Noobxcoders/Alicia",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
