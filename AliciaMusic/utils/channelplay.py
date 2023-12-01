c8680541184336bb94a020d1f7a61ebd4829c4d3
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2023-12-01 21:32:18 +0530

    Update channelplay.py

--- AliciaMusic/utils/channelplay.py
+++ AliciaMusic/utils/channelplay.py
@@ -1,15 +1,5 @@
-#
-# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
-from YukkiMusic import app
-from YukkiMusic.utils.database import get_cmode
+from AliciaMusic import app
+from AliciaMusic.utils.database import get_cmode
 
 
 async def get_channeplayCB(_, command, CallbackQuery):
