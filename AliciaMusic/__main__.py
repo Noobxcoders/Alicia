6fd3b45811a557aeca0c0ed34f4e4086de928f8c
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2023-12-01 21:01:03 +0530

    Update __main__.py

--- AliciaMusic/__main__.py
+++ AliciaMusic/__main__.py
@@ -1,13 +1,3 @@
-#
-# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-#
-
 import asyncio
 import importlib
 import sys
@@ -17,10 +7,10 @@ from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
 from config import BANNED_USERS
-from YukkiMusic import LOGGER, app, userbot
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.plugins import ALL_MODULES
-from YukkiMusic.utils.database import get_banned_users, get_gbanned
+from AliciaMusic import LOGGER, app, userbot
+from AliciaMusic.core.call import Yukki
+from AliciaMusic.plugins import ALL_MODULES
+from AliciaMusic.utils.database import get_banned_users, get_gbanned
 
 loop = asyncio.get_event_loop_policy().get_event_loop()
 
@@ -33,7 +23,7 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER("YukkiMusic").error(
+        LOGGER("AliciaMusic").error(
             "No Assistant Clients Vars Defined!.. Exiting Process."
         )
         return
@@ -41,7 +31,7 @@ async def init():
         not config.SPOTIFY_CLIENT_ID
         and not config.SPOTIFY_CLIENT_SECRET
     ):
-        LOGGER("YukkiMusic").warning(
+        LOGGER("AliciaMusic").warning(
             "No Spotify Vars defined. Your bot won't be able to play spotify queries."
         )
     try:
@@ -55,28 +45,28 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module(f"YukkiMusic.plugins{all_module}")
-    LOGGER("Yukkimusic.plugins").info(
+        importlib.import_module(f"AliciaMusic.plugins{all_module}")
+    LOGGER("AliciaMusic.plugins").info(
         "Successfully Imported Modules "
     )
     await userbot.start()
     await Yukki.start()
     try:
-        await Yukki.stream_call(
+        await Alicia.stream_call(
             "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
         )
     except NoActiveGroupCall:
-        LOGGER("YukkiMusic").error(
+        LOGGER("AliciaMusic").error(
             "[ERROR] - \n\nPlease turn on your Logger Group's Voice Call. Make sure you never close/end voice call in your log group"
         )
         sys.exit()
     except:
         pass
     await Yukki.decorators()
-    LOGGER("YukkiMusic").info("Yukki Music Bot Started Successfully")
+    LOGGER("AliciaMusic").info("Alicia Music Bot Started Successfully")
     await idle()
 
 
 if __name__ == "__main__":
     loop.run_until_complete(init())
-    LOGGER("YukkiMusic").info("Stopping Yukki Music Bot! GoodBye")
+    LOGGER("AliciaMusic").info("Stopping Alicia Music Bot! GoodBye")
