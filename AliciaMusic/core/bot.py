befea60802262666f0df2dcc46033f36697590f5
Noobxcoders <128674386+Noobxcoders@users.noreply.github.com>
2023-12-03 07:27:40 +0530

    Update bot.py

--- AliciaMusic/core/bot.py
+++ AliciaMusic/core/bot.py
@@ -9,11 +9,11 @@ import config
 from ..logging import LOGGER
 
 
-class Alicia(Client):
+class Aliciabot(Client):
     def __init__(self):
         LOGGER(__name__).info("Starting Bot")
         super().__init__(
-            "AliciaMusic",
+            "AliciaMusicbot",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
